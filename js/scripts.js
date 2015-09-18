cd0416da9af4216ec7f9eb58b0b69134af03fd3e
Yelena <ynedelko@gmail.com>
2015-09-18 09:10:18 -0700

    Create new instances of pizza orders, given user's specifications, with the Pizza constructor.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Pizza(pizzaSize, toppingNumber, toppingName, pizzaQuantity) {
+  this.pizzaSize = pizzaSize;
+  this.toppingNumber = toppingNumber;
+  this.toppingName = toppingName;
+  this.pizzaQuantity = pizzaQuantity;
+  this.startPrice = 5;
+}
---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+describe('Pizza', function () {
+  it("create a new Pizza order given user's specifications", function() {
+    var testPizza = new Pizza("small", 2, "cheese and mushroom", 2);
+    expect(testPizza.pizzaSize).to.equal("small");
+    expect(testPizza.toppingNumber).to.equal(2);
+    expect(testPizza.toppingName).to.equal("cheese and mushroom");
+    expect(testPizza.pizzaQuantity).to.equal(2);
+  });
+});
