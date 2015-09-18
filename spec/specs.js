ce4dd2b0d395e0bf2a30c046d5454cc649043afd
Yelena <ynedelko@gmail.com>
2015-09-18 11:29:09 -0700

    Re-add specs that disappered.

--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,4 @@
-describe('Pizza', function () {
+describe('Pizza', function() {
   it("create a new Pizza order given user's specifications", function() {
     var testPizza = new Pizza("small", 2, "cheese and mushroom", 2);
     expect(testPizza.pizzaSize).to.equal("small");
@@ -6,15 +6,18 @@ describe('Pizza', function () {
     expect(testPizza.toppingName).to.equal("cheese and mushroom");
     expect(testPizza.pizzaQuantity).to.equal(2);
   });
-  it("calculate the pizza order cost for a small pizza, considering the number of toppings and quantity of pizzas", function () {
+
+  it("calculate the pizza order cost for a small pizza, considering the number of toppings and quantity of pizzas", function() {
     var testPizza = new Pizza("small", 2, "cheese and mushroom", 2);
     expect(testPizza.pizzaPrice()).to.equal(14);
   });
-  it("calculate the pizza order cost for a medium pizza, considering the number of toppings and quantity of pizzas", function () {
+
+  it("calculate the pizza order cost for a medium pizza, considering the number of toppings and quantity of pizzas", function() {
     var testPizza = new Pizza("medium", 1, "cheese", 3);
     expect(testPizza.pizzaPrice()).to.equal(33);
   });
-  it("calculate the pizza order cost for a large pizza, considering the number of toppings and quantity of pizzas", function () {
+
+  it("calculate the pizza order cost for a large pizza, considering the number of toppings and quantity of pizzas", function() {
     var testPizza = new Pizza("large", 2, "cheese and mushroom", 5);
     expect(testPizza.pizzaPrice()).to.equal(85);
   });
