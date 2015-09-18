5fccfb5e99d91a188aeb2282cdab9a536e660171
Yelena <ynedelko@gmail.com>
2015-09-18 11:19:57 -0700

    Add web views and jQuery functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,18 @@ Pizza.prototype.pizzaPrice = function() {
   }
     return this.startPrice * this.pizzaQuantity;
 };
+
+$(document).ready(function() {
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+
+    var chosenPizzaSize = $("input[name=size-choice]:checked", "#pizza-order").val();
+    var chosenToppingNumber = $("input#topping-number").val();
+    var inputtedToppingName = $("input#topping-name").val();
+    var chosenPizzaQuantity = $("input#pizza-quantity").val();
+
+    var newPizzaOrder = new Pizza (chosenPizzaSize, chosenToppingNumber, inputtedToppingName, chosenPizzaQuantity);
+    var newPizzaOrderPrice = newPizzaOrder.pizzaPrice();
+    $("ul#orders").append("<li><span class='place'>" + "Your Price: " + "$" + newPizzaOrderPrice + ".00" + "</span></li>");
+  });
+});
